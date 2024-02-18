95b0fbb1e6d8295d2fa9b0fc6dd820823ddcbdcf
Dhruv Baraiya <dhruvbaraiya27@gmail.com>
2024-02-18 19:37:13 +0530

    Initial commit

---
+++ src/main/java/com/documents/model/UserDetails.java
@@ -0,0 +1,21 @@
+package com.documents.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String number;
+    private String studentId;
+}
