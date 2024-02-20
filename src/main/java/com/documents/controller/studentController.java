f1345e95e85d1ddacffa07760f5a2d789732920e
b202001162 <202001162@daiict.ac.in>
2024-02-20 12:34:37 +0530

    Yes

--- src/main/java/com/documents/controller/studentController.java
+++ src/main/java/com/documents/controller/studentController.java
@@ -3,6 +3,7 @@ package com.documents.controller;
 import com.documents.model.studentDetails;
 import com.documents.repository.studentRepository;
 import com.documents.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,7 @@ public class studentController {
     @PostMapping("/login")
     public ResponseEntity<studentDetails> createUser(@RequestBody studentDetails studentDetails)
     {
+
         studentDetails studentDetails1 =userService.createUser(studentDetails);
         return new ResponseEntity<>(studentDetails1, HttpStatus.OK);
 
